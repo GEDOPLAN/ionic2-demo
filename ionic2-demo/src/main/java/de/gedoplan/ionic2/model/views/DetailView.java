4ab8c62e319d2512004f83de721110ae24f5b9d2
Dominik Mathmann <dominik.mathmann@gedoplan.de>
2016-08-30 10:44:43 +0200

    java ee backend

---
+++ ionic2-demo/src/main/java/de/gedoplan/ionic2/model/views/DetailView.java
@@ -0,0 +1,6 @@
+
+package de.gedoplan.ionic2.model.views;
+
+public class DetailView extends ListView {
+
+}
---
+++ ionic2-demo/src/main/java/de/gedoplan/ionic2/resource/CustomerResource.java
@@ -0,0 +1,34 @@
+package de.gedoplan.ionic2.resource;
+
+import com.fasterxml.jackson.annotation.JsonView;
+import de.gedoplan.ionic2.model.Customer;
+import de.gedoplan.ionic2.model.views.ListView;
+import de.gedoplan.ionic2.repository.CustomerRepository;
+import java.util.List;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ *
+ * @author Dominik Mathmann
+ */
+@ApplicationScoped
+@Path("customer")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class CustomerResource {
+
+    @Inject
+    private CustomerRepository customerRepository;
+
+    @GET
+    @JsonView(ListView.class)
+    public List<Customer> getCustomers() {
+        return customerRepository.getAll();
+    }
+}
